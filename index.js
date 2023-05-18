8bef104e3141140174928fd3012f915aaae9993f
samy01010 <115762968+samy01010@users.noreply.github.com>
2023-05-18 21:59:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// scrol section active link
+
+let Selection = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    Sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
